e520507fa4c822cee588a5186dcadd3dc49ce475
Abraham Diaw <abrahamdiaw@macbook-pro-de-abraham.home>
2022-11-07 18:34:20 +0100

    add cart && cart service && shipping component

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Product, products } from 'src/products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,13 +13,24 @@ export class ProductDetailsComponent implements OnInit {
 
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute){}
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) { }
 
-  ngOnInit(){
-      const routeParams = this.route.snapshot.paramMap;
-      const productIdFromRoute = Number(routeParams.get('productId'));
-      console.log(routeParams, productIdFromRoute);
+  ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+    console.log(routeParams, productIdFromRoute);
 
-      this.product = products.find(product => product.id === productIdFromRoute);
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    alert('Your product has been added to the cart!');
+    console.log(this.cartService.getItems());
+
+  }
+
 }
